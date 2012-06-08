de2400aeddad6d9957528a873d2ede696aadf449
ntksh <n.tksh2@gmail.com>
2012-06-08 23:59:27 +0900

    自然数のベキ乗を求める関数 power の目的とテストを作成

---
+++ src/ch11/power.ml
@@ -0,0 +1,13 @@
+(* 目的：自然数 m と n を受け取り、m の n 乗を求める *)
+(* power : int -> int -> int *)
+let rec power m n = 
+    if n = 0 then 0
+             else 0
+
+
+
+(* テスト *)
+let test1 = power 3 0 = 1
+let test2 = power 3 1 = 3
+let test3 = power 3 2 = 9
+let test4 = power 3 3 = 27
