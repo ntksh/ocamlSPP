3060a445c6ddc1e24d8947fb8c642782e69c99c6
ntksh <n.tksh2@gmail.com>
2012-06-08 18:57:38 +0900

    even のテストを作成

--- src/ch9/even.ml
+++ src/ch9/even.ml
@@ -7,3 +7,12 @@
  * *)
 let even lst = []
 
+
+(* テスト *)
+let test1 = even [] = []
+let test2 = even [2] = [2]
+let test3 = even [1] = []
+let test4 = even [1; 2] = [2]
+let test5 = even [1; 3] = []
+let test6 = even [1; 3; 4] = [4]
+let test7 = even [2; 4; 6; 7] = [2; 4; 6]
