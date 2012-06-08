019d50913d715ad89214cfead20a38d8e3ad9e09
ntksh <n.tksh2@gmail.com>
2012-06-08 21:22:42 +0900

    関数 insert の目的を作成

---
+++ src/ch10/insert.ml
@@ -0,0 +1,8 @@
+(* int list は
+ *   - []             空のリスト、または
+ *   - first :: rest  first は整数型, rest は残りのリスト
+ *   という形になる。 *)
+
+(* 目的：昇順に並んでいる整数のリスト lst と整数 n を受け取り、昇順となる市に n を挿入したリストを返す *)
+(* insert : int list int -> int list *)
+let rec insert lst n = []
