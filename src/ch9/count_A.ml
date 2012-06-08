04f1438fd457d3edb8bf7d8a9bcc1c9e60a03973
ntksh <n.tksh2@gmail.com>
2012-06-08 19:38:38 +0900

    関数 count_A に 成績の判定ロジックを追加

--- src/ch9/count_A.ml
+++ src/ch9/count_A.ml
@@ -25,7 +25,9 @@ let lst4 = [{namae = "yoshida"; tensuu = 80; seiseki = "A"};
 (* count_A : gakusei_t list -> int *)
 let count_A lst = match lst with 
         [] -> 0
-     |  {namae = n; tensuu = t; seiseki = s} :: rest -> 0 (* count_A rest *)
+     |  {namae = n; tensuu = t; seiseki = s} :: rest
+         -> if s = "A" then 0
+                       else 0
 
 
 (* テスト *)
