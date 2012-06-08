a53053008fc33b71dec1de39d1515d4701d68bb5
ntksh <n.tksh2@gmail.com>
2012-06-08 18:45:45 +0900

    関数 length のテストを作成

--- src/ch9/lentgh.ml
+++ src/ch9/lentgh.ml
@@ -6,3 +6,10 @@
 (* 目的：整数のリスト lst を受け取ったら、そのリストの長さを返す *)
 (* length : int list -> int *)
 let length lst = 0
+
+
+(* テスト *)
+let test1 = length [] = 0
+let test2 = length [1] = 1
+let test3 = length [1; 2] = 2
+let test4 = length [1; 2; 3; 4; 5; 6; 7] = 7
