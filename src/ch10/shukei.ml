e8d10eb3fe9e02b850bb16691dcb06abfa898057
ntksh <n.tksh2@gmail.com>
2012-06-08 22:57:16 +0900

    関数 shukei にパターンマッチ付き局所変数定義を導入

--- src/ch10/shukei.ml
+++ src/ch10/shukei.ml
@@ -3,9 +3,8 @@
 let rec shukei lst = match lst with
     [] -> (0, 0, 0, 0)
   | {namae = n; tensuu = t; seiseki = s} :: rest ->
-          let shukei_rest = shukei rest in
-          match shukei_rest with
-          (a, b, c, d) -> if s = "A" then (a + 1, b, c, d)
-                        else if s = "B" then (a, b + 1, c, d)
-                        else if s = "C" then (a, b, c + 1, d)
-                        else (a, b, c, d + 1)
+          let (a, b, c, d) = shukei rest in
+          if s = "A" then (a + 1, b, c, d)
+          else if s = "B" then (a, b + 1, c, d)
+          else if s = "C" then (a, b, c + 1, d)
+          else (a, b, c, d + 1)
