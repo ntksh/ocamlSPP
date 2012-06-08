6ca77b737aa4f574512998312d9c323e113fde99
ntksh <n.tksh2@gmail.com>
2012-06-08 21:25:18 +0900

    関数 insert のテストを作成

--- src/ch10/insert.ml
+++ src/ch10/insert.ml
@@ -6,3 +6,11 @@
 (* 目的：昇順に並んでいる整数のリスト lst と整数 n を受け取り、昇順となる市に n を挿入したリストを返す *)
 (* insert : int list int -> int list *)
 let rec insert lst n = []
+
+
+(* テスト *)
+let test1 = insert [] 0 = [0]
+let test2 = insert [0] 1 = [0; 1]
+let test3 = insert [0] 0 = [0; 0]
+let test4 = insert [0; 1; 2] 1 = [0; 1; 1; 2]
+let test5 = insert [0; 1; 2; 4] 3 = [0; 1; 2; 3; 4]
