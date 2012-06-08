43f7a26d3dd17212bb38bd84823fc36d9d5c9176
ntksh <n.tksh2@gmail.com>
2012-06-08 21:48:05 +0900

    関数 ins_sort のテストを作成

--- src/ch10/ins_sort.ml
+++ src/ch10/ins_sort.ml
@@ -1,3 +1,12 @@
 (* 目的：整数のリストを受け取り、昇順に整列したリストを返す *)
 (* ins_sort : int list -> int list *)
 let rec ins_sort lst = []
+
+(* テスト *)
+let test1 = ins_sort [] = []
+let test2 = ins_sort [1] = [1]
+let test3 = ins_sort [1; 2] = [1; 2]
+let test4 = ins_sort [2; 1] = [1; 2]
+let test5 = ins_sort [1; 2; 3] = [1; 2; 3]
+let test6 = ins_sort [1; 3; 2] = [1; 2; 3]
+let test7 = ins_sort [3; 1; 2] = [1; 2; 3]
