33f0c861982da3fd81096ce44dbdb035fea10689
ntksh <n.tksh2@gmail.com>
2012-06-08 18:48:39 +0900

    関数 length のテンプレートを記載、本体の作成

---
+++ src/ch9/length.ml
@@ -0,0 +1,17 @@
+(* int list は
+ *    - []             空リスト、あるいは
+ *    - first :: rest  最初の要素が first で、残りのリストが rest
+ *   という形 *)
+
+(* 目的：整数のリスト lst を受け取ったら、そのリストの長さを返す *)
+(* length : int list -> int *)
+let rec length lst = match lst with
+    [] -> 0
+  | first :: rest -> 1 + length rest
+
+
+(* テスト *)
+let test1 = length [] = 0
+let test2 = length [1] = 1
+let test3 = length [1; 2] = 2
+let test4 = length [1; 2; 3; 4; 5; 6; 7] = 7
