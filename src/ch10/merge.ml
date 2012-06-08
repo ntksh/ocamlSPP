6f2ef27c0f52e0d2ab9da4f2ef12ef965dc3513e
ntksh <n.tksh2@gmail.com>
2012-06-08 23:13:51 +0900

    関数 merge のテストを作成

--- src/ch10/merge.ml
+++ src/ch10/merge.ml
@@ -8,3 +8,11 @@ let rec merge lst1 lst2 = match (lst1, lst2) with
           if first1 < first2
           then []
           else []
+
+(* テスト *)
+let test1 = merge [] [] = []
+let test2 = merge [] [1; 2] = [1; 2]
+let test3 = merge [1; 2] [] = [1; 2]
+let test4 = merge [1; 3] [2; 4] = [1; 2; 3; 4]
+let test5 = merge [2; 4] [1; 3] = [1; 2; 3; 4]
+let test6 = merge [1; 4] [1; 3] = [1; 1; 3; 4]
