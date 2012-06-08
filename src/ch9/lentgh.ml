dabecbfc798125f7da6965826eac397b2eceeb20
ntksh <n.tksh2@gmail.com>
2012-06-08 18:43:28 +0900

    関数 length の目的を確認

--- src/ch9/lentgh.ml
+++ src/ch9/lentgh.ml
@@ -2,3 +2,7 @@
  *    - []             空リスト、あるいは
  *    - first :: rest  最初の要素が first で、残りのリストが rest
  *   という形 *)
+
+(* 目的：整数のリスト lst を受け取ったら、そのリストの長さを返す *)
+(* length : int list -> int *)
+let length lst = 0
