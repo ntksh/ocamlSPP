65f31e8b2d6508de5e8a39d18aea6fc49c4d834b
ntksh <n.tksh2@gmail.com>
2012-06-08 17:33:23 +0900

    関数のプロトタイプを作成

--- src/ch9/contain_zero.ml
+++ src/ch9/contain_zero.ml
@@ -2,3 +2,7 @@
  *     - []             空リスト、あるいは
  *     - first :: rest  最初の要素が first で、残りのリストが rest
  *     という形 *)
+
+(* 目的：受け取ったリスト lst に 0 が含まれているかを調べる *)
+(* contain_zero : int list -> bool *)
+let contain_zero lst = false
