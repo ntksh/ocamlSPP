930443245467797920eb57d5ce92c9bb65913f92
ntksh <n.tksh2@gmail.com>
2012-06-17 03:56:05 +0900

    関数 total_distance の定義を局所関数を用いて作成

--- src/ch16/total_distance.ml
+++ src/ch16/total_distance.ml
@@ -4,15 +4,17 @@ type distance_t = {
     total : float; (* 距離の合計 *)
 }
 
+
 (* 目的：先頭からリスト中の各点までの距離の合計を計算する *)
-(* ここで total0 はこれまでの距離の合計 *)
-(* hojo : distance_t list -> float -> distance_t list *)
-let rec hojo lst total0 = match lst with
+(* total_distance : distance_t list -> distance_t list *)
+let rec total_distance lst = 
+    (* 目的：先頭からリスト中の各点までの距離の合計を計算する *)
+    (* ここで total0 はこれまでの距離の合計 *)
+    (* hojo : distance_t list -> float -> distance_t list *)
+    let rec hojo lst total0 = match lst with
         [] -> []
       | {kyori = k; total = t} :: rest ->
               {kyori = k; total = total0 +. k} 
                :: hojo rest (total0 +. k)
+    in hojo lst 0.0
 
-(* 目的：先頭からリスト中の各点までの距離の合計を計算する *)
-(* total_distance : distance_t list -> distance_t list *)
-let rec total_distance lst = hojo lst 0.0 
