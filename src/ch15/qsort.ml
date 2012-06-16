a3575f3f922771062e2ce6412088de74944cde37
ntksh <n.tksh2@gmail.com>
2012-06-17 02:55:08 +0900

    関数 quick_sort の定義を局所関数定義を使うよう変更

--- src/ch15/qsort.ml
+++ src/ch15/qsort.ml
@@ -14,7 +14,25 @@ let take_greater n lst = take n lst (>)
 
 (* 目的：受け取った lst をクイックソートを使って照準に整列する *)
 (* quick_sort : int list -> int list *)
-let rec quick_sort lst = match lst with
+let rec quick_sort lst = 
+    (* 目的：lst の中から n より p である要素のみを取り出す *) 
+    (* take :int -> int list -> (int -> int -> bool) -> int list *)
+    let take n lst p = List.filter (fun item -> p item n) lst
+    (* 目的：lst の中から n より小さい要素のみを取り出す *)
+    (* take_less : int -> int list -> int list *)
+    in let take_less n lst = take n lst (<) 
+    (* 目的：lst の中から n より大きい要素のみを取り出す *)
+    (* take_greater : int -> int list -> int list *)
+    in let take_greater n lst = take n lst (>) 
+    in match lst with
+          [] -> []
+        | first :: rest -> quick_sort (take_less first rest)
+                           @ [first]
+                           @ quick_sort (take_greater first rest)
+    
+    
+    
+    match lst with
     [] -> [] (* 自明に答えが出るケース *)
   | first :: rest -> quick_sort (take_less first rest)
                      @ [first]
