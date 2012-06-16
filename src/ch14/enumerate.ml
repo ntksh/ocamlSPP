03ad1a9628d82b2944fd27ee1f6526bdcc243ad7
ntksh <n.tksh2@gmail.com>
2012-06-16 18:20:16 +0900

    関数enumerate のテンプレートを作成

---
+++ src/ch14/enumerate.ml
@@ -0,0 +1,9 @@
+(* 目的：n から 1 までのリストを作る *)
+(* enumerate : int -> int list *)
+let enumerate n = []
+
+
+(* テスト *)
+let test1 = enumerate 0 = []
+let test2 = enumerate 1 = [1]
+let test3 = enumerate 2 = [2; 1]
