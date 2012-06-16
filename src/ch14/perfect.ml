3fd1b9de822944a891322c346f7f1f002a7361db
ntksh <n.tksh2@gmail.com>
2012-06-16 18:39:36 +0900

    関数perfect のテンプレートを作成

---
+++ src/ch14/perfect.ml
@@ -0,0 +1,10 @@
+(* 目的：m 以下の完全数のリストを返す *)
+(* perfect : int -> int list *)
+let perfect m = []
+
+
+(* テスト *)
+let test1 = perfect 0 = []
+let test2 = perfect 1 = []
+let test3 = perfect 2 = []
+let test4 = perfect 6 = [6]
