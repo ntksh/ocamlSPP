b39987d70a974df1e6c26ccbaecebc1ad510f42c
ntksh <n.tksh2@gmail.com>
2012-06-17 02:09:13 +0900

    関数 quick_sort のテンプレートを作成

---
+++ src/ch15/qsort.ml
@@ -0,0 +1,11 @@
+(* 目的：受け取った lst をクイックソートを使って照準に整列する *)
+(* quick_sort : int list -> int list *)
+let rec quick_sort lst = []
+
+
+(* テスト *)
+let test1 = quick_sort [] = []
+let test2 = quick_sort [1] = [1]
+let test3 = quick_sort [1; 2] = [1; 2]
+let test4 = quick_sort [2; 1] = [1; 2]
+let test5 = quick_sort [5; 4; 9; 8; 2; 3] = [2; 3;4; 5; 8; 9];
