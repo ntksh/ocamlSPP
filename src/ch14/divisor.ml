1d77e32e8bc45431de5f19ee5ab7617c01101cb8
ntksh <n.tksh2@gmail.com>
2012-06-16 18:34:52 +0900

    List.filter の追記

--- src/ch14/divisor.ml
+++ src/ch14/divisor.ml
@@ -3,13 +3,13 @@
 (* 目的：n の約数のリストを返す *)
 (* divisor : int -> int list *)
 let divisor n = 
-    filter (fun x -> n mod x = 0) (enumerate n)
+    List.filter (fun x -> n mod x = 0) (enumerate n)
 
 
 (* テスト *)
 let test1 = divisor 0 = []
 let test2 = divisor 1 = [1]
-let test3 = divisor 2 = [1; 2]
-let test4 = divisor 4 = [1; 2; 4]
-let test5 = divisor 6 = [1; 2; 3; 6]
-let test6 = divisor 9 = [1; 3; 9]
+let test3 = divisor 2 = [2; 1]
+let test4 = divisor 4 = [4; 2; 1]
+let test5 = divisor 6 = [6; 3; 2; 1]
+let test6 = divisor 9 = [9; 3; 1]
