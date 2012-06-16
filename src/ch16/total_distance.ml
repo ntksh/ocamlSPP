67b3475492f6e185c3cf00b41d4194c881014d0f
ntksh <n.tksh2@gmail.com>
2012-06-17 03:36:13 +0900

    これまでの距離の合計という引数を増やした補助関数 hojo を作成

--- src/ch16/total_distance.ml
+++ src/ch16/total_distance.ml
@@ -4,6 +4,13 @@ type distance_t = {
     total : float; (* 距離の合計 *)
 }
 
+(* 目的：先頭からリスト中の各点までの距離の合計を計算する *)
+(* ここで total0 はこれまでの距離の合計 *)
+(* hojo : distance_t list -> float -> distance_t list *)
+let rec hojo lst total0 = match lst with
+        [] -> []
+      | {kyori = k; total = t} :: rest ->
+              [] (* hojo rest *)
 
 (* 目的：先頭からリスト中の各点までの距離の合計を計算する *)
 (* total_distance : distance_t list -> distance_t list *)
