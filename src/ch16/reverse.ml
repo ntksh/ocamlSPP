92f141ab57150e47fdb3f37fede67bbe72f8a866
ntksh <n.tksh2@gmail.com>
2012-06-17 13:59:39 +0900

    関数 reverse の定義を局所関数定義を用いて作成

--- src/ch16/reverse.ml
+++ src/ch16/reverse.ml
@@ -6,6 +6,10 @@ let rec rev lst result = match lst with
 
 (* 目的：与えられたリストを逆順にして返す *)
 (* reverse : 'a list -> 'a list *)
-let rec reverse lst = match lst with
-        [] -> []
-     |  first :: rest -> [] (* reverse rest *)
+let rec reverse lst =
+        (* 目的：(lst の逆順のリスト) @ result を返す *)
+        (* ここで result はこれまでの要素を逆順にしたリストを示す *)
+        let rec rev lst result = match lst with 
+            [] -> result 
+          | first :: rest -> rev rest (first :: rest)
+        in rev lst []
