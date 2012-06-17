f51fbe6354befa198906de1e1f852d41cf8f5b1a
ntksh <n.tksh2@gmail.com>
2012-06-17 13:53:10 +0900

    補助関数 rev のテンプレートを作成

--- src/ch16/reverse.ml
+++ src/ch16/reverse.ml
@@ -1,3 +1,9 @@
+(* 目的：(lst の逆順のリスト) @ result を返す *)
+(* ここで result はこれまでの要素を逆順にしたリストを示す *)
+let rec rev lst result = match lst with 
+        [] -> []
+      | first :: rest -> [] (* rev rest *)
+
 (* 目的：与えられたリストを逆順にして返す *)
 (* reverse : 'a list -> 'a list *)
 let rec reverse lst = match lst with
