48fe759ef9b8c5cb8f7e98eb7009de0f5ec7e882
ntksh <n.tksh2@gmail.com>
2012-06-24 17:08:16 +0900

    問題4.6の解答を作成

--- src/ex/ex4_6.ml
+++ src/ex/ex4_6.ml
@@ -1,8 +1,8 @@
 (* 目的：鶴の数を与えたら、足の本数を返す *)
 (* tsuru_no_ashi : int -> int *)
-let tsuru_no_ashi num = 0
+let tsuru_no_ashi num = num * 2
 
 (* テスト *)
-let test1 = tsuru_no_ahis 0 = 0
+let test1 = tsuru_no_ashi 0 = 0
 let test2 = tsuru_no_ashi 1 = 2
 let test3 = tsuru_no_ashi 4 = 8
