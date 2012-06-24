07776a8cd244a8b7dcbe29e974875a21d72a2726
ntksh <n.tksh2@gmail.com>
2012-06-24 17:09:59 +0900

    問題4.6に kame_no_ashi を作成

--- src/ex/ex4_6.ml
+++ src/ex/ex4_6.ml
@@ -6,3 +6,12 @@ let tsuru_no_ashi num = num * 2
 let test1 = tsuru_no_ashi 0 = 0
 let test2 = tsuru_no_ashi 1 = 2
 let test3 = tsuru_no_ashi 4 = 8
+
+(* 目的：亀の数を与えたら、足の本数を返す *)
+(* kame_no_ashi : int -> int *)
+let kame_no_ashi num = num * 2
+
+(* テスト *)
+let test4 = kame_no_ashi 0 = 0
+let test5 = kame_no_ashi 1 = 2
+let test6 = kame_no_ashi 4 = 8
